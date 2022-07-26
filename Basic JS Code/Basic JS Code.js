4366dd740ccee7cc0755cebb6ccb48d3aba43618
Suman Sourabh <71708709+Sumansourabh14@users.noreply.github.com>
2022-07-26 23:43:37 +0530

    Added Fibonacci Sequence program!

--- Basic JS Code/Basic JS Code.js
+++ Basic JS Code/Basic JS Code.js
@@ -29,3 +29,22 @@ console.log(typeof c);
 
 //
 console.log(Math.round(Math.pow(1000, 1/3)));
+
+
+// Fibonacci Series - The Fibonacci Sequence is the series of numbers: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ... The next number is found by adding up the two numbers before it
+console.log("Fibonacci Sequence Starts:");
+let firstNumber = 0;
+let secondNumber = 1;
+let limit = 1;
+
+console.log(firstNumber);
+console.log(secondNumber);
+
+while (limit <= 6) {
+    let thirdNumber = firstNumber + secondNumber;
+    firstNumber = secondNumber;
+    secondNumber = thirdNumber;
+    console.log(thirdNumber);
+    limit++;
+}
+console.log("Fibonacci Sequence Ends.");
